452f7c0be9dbc639f0bc627bc5d1807b2944647a
EvaChan <as45986@gmail.com>
2024-11-01 13:08:15 +0800

    fix:use next14, update dependencies and improve PostPage component

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,3 @@
-'use client'; // 標示為客戶端組件
-import * as React from 'react';
 import Mdx from '@/components/mdx-components';
 import { allPosts } from 'contentlayer/generated';
 import { format, parseISO } from 'date-fns';
@@ -11,16 +9,20 @@ interface PostPageProps {
   };
 }
 
-// 將 PostPage 改為 async 函數
-export default function PostPage({ params }: { params: Promise<PostPageProps['params']> }) {
-  // 使用 React.use() 解構 params
-  const { slug } = React.use(params);
+// Convert PostPage into an async function
+export default  function PostPage({ params }: PostPageProps) {
+  // Await params to access its properties
+  const { slug } = params;
 
+  // Find the post matching the slug
   const post = allPosts.find((post) => post._raw.flattenedPath === slug);
 
+  // Handle case where post is not found
   if (!post) {
     return <div>No post here!</div>;
   }
+
+  // Render the post content
   return (
     <article className='py-8 mx-auto max-w-xl'>
       <div className='mb-8 text-center'>
@@ -30,10 +32,6 @@ export default function PostPage({ params }: { params: Promise<PostPageProps['pa
         <h1>{post.title}</h1>
       </div>
       <Mdx code={post.body.code} />
-      {/* <div
-        className='text-sm [&>*]:mb-3 [&>*:last-child]:mb-0'
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      /> */}
     </article>
   );
 }
